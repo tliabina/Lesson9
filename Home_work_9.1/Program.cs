2a86545f6c13648ef3a47b4d339253da076fd37f
Tatiana Liabina <t.liabina@icloud.com>
2022-12-07 10:19:10 +0400

    add tasks

---
+++ Home_work_9.1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNum(n,1));
+
+string PrintNum(int start, int end)
+{
+    if (start==end)
+    {
+        return end.ToString();
+    }
+    return(start+ " "+PrintNum(start -1,  end));
+   
+
+
+}
---
+++ Home_work_9.2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.WriteLine("Введите начальное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNum(n,m));
+
+string PrintNum(int start, int end)
+{
+    if (start==end)
+    {
+        return end.ToString();
+    }
+    return(start+ " "+PrintNum(start +1,  end));
+}
+
+int PrintSum (int s)
+{
+if (n==m)
+    {
+        return m;
+    }
+    
+    int sum=0;
+    sum = n +(n+1);
+    return (PrintSum (s));
+
+}
+
+
